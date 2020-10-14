16e73a1f9f7357dc057429c22444d8ca3a6982a4
yersson <yersson2@gmail.com>
2020-10-14 16:49:01 -0500

    fifth commit

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,17 +1,18 @@
 import { Router } from 'express';
 import app from '../app';
 import * as productsCtrl from '../controllers/products.controller';
+import { authJwt } from '../middlewares';
 
 const router = Router();
 
-router.post('/', productsCtrl.createProduct);
+router.post('/', [authJwt.verifyToken, authJwt.isModerator], productsCtrl.createProduct);
 
 router.get('/', productsCtrl.getProducts);
 
 router.get('/:productId', productsCtrl.getProductById);
 
-router.put('/:productId', productsCtrl.updateProductById);
+router.put('/:productId', [authJwt.verifyToken, authJwt.isAdmin], productsCtrl.updateProductById);
 
-router.delete('/:productId', productsCtrl.deleteProductById);
+router.delete('/:productId', [authJwt.verifyToken , authJwt.isAdmin], productsCtrl.deleteProductById);
 
 export default router;
