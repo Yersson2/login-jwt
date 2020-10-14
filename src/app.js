17ff4b13f65c264a4e71fb8aab19f8e9c9917fef
yersson <yersson2@gmail.com>
2020-10-14 10:41:18 -0500

    fourth commit

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import morgan from 'morgan';
 import pkg from '../package.json';
+import {createRoles} from './libs/initialSetup';
 import productsRouters from './routes/products.routes';
 import authRoutes from './routes/auth.routes';
 
 const app = express();
+createRoles();
 
 app.set('pkg', pkg);
 
